6e7f82466fb0e64e059305d4f6a2ab76ed5b528c
tingamapuro04 <alponcemobutu100@gmail.com>
2022-11-06 00:31:29 +0300

    Linter errors

--- main.js
+++ main.js
@@ -9,40 +9,34 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-const pics = [["pic1.jpg", "First pic"], ["pic2.jpg", "Second photo"], ["pic3.jpg", "Third photo"], ["pic4.jpg", "Fourth photo"], ["pic5.jpg", "Fifth photo"]]
-console.log(pics)
+const pics = [['pic1.jpg', 'First pic'], ['pic2.jpg', 'Second photo'], ['pic3.jpg', 'Third photo'], ['pic4.jpg', 'Fourth photo'], ['pic5.jpg', 'Fifth photo']];
 
-pics.map(item => {
+pics.map((item) => {
   const newImage = document.createElement('img');
-  newImage.setAttribute('class', 'mapicha')
+  newImage.setAttribute('class', 'mapicha');
   newImage.setAttribute('src', `images/${item[0]}`);
   newImage.setAttribute('alt', `${item[1]}`);
-  thumbBar.appendChild(newImage);
-})
-
+  return thumbBar.appendChild(newImage);
+});
 
 /* Clicking a single image item and setting it to be the image to be dispalyed at the top */
 
-const mapicha = document.querySelectorAll('.mapicha')
+const mapicha = document.querySelectorAll('.mapicha');
 
 const changepic = (item) => {
-  displayedImage.setAttribute('src', `${item.target.src}`)
-}
+  displayedImage.setAttribute('src', `${item.target.src}`);
+};
 
 // mapicha[0].addEventListener('click', changepic)
 
 const looper = (item) => {
-  item.addEventListener('click', changepic)
-}
+  item.addEventListener('click', changepic);
+};
 
-mapicha.forEach(looper)
-
-
-
-
+mapicha.forEach(looper);
 
 /* Wiring up the Darken/Lighten button */
 
 btn.addEventListener('click', () => {
-  overlay.style.display = 'block'
-})
+  overlay.style.display = 'block';
+});
